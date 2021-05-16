949972fd12e23928eabc18909565cb90f1a20d92
Meema <78662983+japandotorg@users.noreply.github.com>
2021-05-16 10:09:36 +0530

    added anime search command ❤🧡💛
    Made an anime search command which searches anime from mal-rewrite and posts them in the channel after someone searches for an anime, and this command totally works because of mal-scraper

--- src/commands/anime/AnimesearchCommand.js
+++ src/commands/anime/AnimesearchCommand.js
@@ -1,11 +1,58 @@
 const BaseCommand = require('../../utils/structures/BaseCommand');
+const Discord = require('discord.js');
+const Scraper = require("mal-scraper");
 
 module.exports = class AnimesearchCommand extends BaseCommand {
   constructor() {
-    super('animesearch', 'anime', []);
+    super('animesearch', 'anime', ['anime', 'anisearch']);
   }
 
   run(client, message, args) {
-    message.channel.send('animesearch command works');
+     //Start
+    
+     let Text = args[0];
+    
+     if (!Text) return message.channel.send(`Please Give Something!`);
+ 
+     if (Text.length > 200) return message.channel.send(`Text Limit - 200`);
+ 
+     let Msg = await message.channel.send(`**Searching It For You 🔮**`);
+ 
+     let Replaced = Text.replace(/ +/g, " ");
+ 
+     await Msg.delete();
+ 
+     let Anime;
+ 
+     let Embed;
+ 
+     try {
+ 
+       Anime = await Scraper.getInfoFromName(Replaced);
+ 
+       if (!Anime.genres[0] || Anime.genres[0] === null) Anime.genres[0] = "None";
+ 
+       Embed = new Discord.MessageEmbed()
+         .setColor('#2f3136')
+         .setURL(Anime.url)
+         .setTitle(Anime.title)
+         .setDescription(Anime.synopsis)
+         .addField(`Type`, Anime.type, true)
+         .addField(`Status`, Anime.status, true)
+         .addField(`Premiered`, Anime.premiered, true)
+         .addField(`Episodes`, Anime.episodes, true)
+         .addField(`Duration`, Anime.duration, true)
+         .addField(`Popularity`, Anime.popularity, true)
+         .addField(`Gneres`, Anime.genres.join(", "))
+         .setThumbnail(Anime.picture)
+         .setFooter(`Score - ${Anime.score}`)
+         .setTimestamp();
+ 
+     } catch (error) {
+       return message.channel.send(`No Anime Found!`);
+     };
+ 
+     return message.channel.send(Embed);
+     //End
   }
 }
