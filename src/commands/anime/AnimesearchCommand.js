3468a4cfbaf7d990412ea02d88ade32cbfc6cb88
Meema <78662983+japandotorg@users.noreply.github.com>
2021-05-16 17:00:51 +0530

    Added an userinfo command and formatted all the other commands 💞
    Added an user-info command with permission checking, and formatted every other command according to javascript formation

--- src/commands/anime/AnimesearchCommand.js
+++ src/commands/anime/AnimesearchCommand.js
@@ -8,51 +8,51 @@ module.exports = class AnimesearchCommand extends BaseCommand {
   }
 
   async run(client, message, args) {
-     //Start
-    
-     let Text = args[0];
-    
-     if (!Text) return message.channel.send(`Please Give Something!`);
- 
-     if (Text.length > 200) return message.channel.send(`Text Limit - 200`);
- 
-     let Msg = await message.channel.send(`**Searching It For You 🔮**`);
- 
-     let Replaced = Text.replace(/ +/g, " ");
- 
-     await Msg.delete();
- 
-     let Anime;
- 
-     let Embed;
- 
-     try {
- 
-       Anime = await Scraper.getInfoFromName(Replaced);
- 
-       if (!Anime.genres[0] || Anime.genres[0] === null) Anime.genres[0] = "None";
- 
-       Embed = new Discord.MessageEmbed()
-         .setColor('#2f3136')
-         .setURL(Anime.url)
-         .setTitle(Anime.title)
-         .setDescription(Anime.synopsis)
-         .addField(`Type`, Anime.type, true)
-         .addField(`Status`, Anime.status, true)
-         .addField(`Premiered`, Anime.premiered, true)
-         .addField(`Episodes`, Anime.episodes, true)
-         .addField(`Duration`, Anime.duration, true)
-         .addField(`Popularity`, Anime.popularity, true)
-         .addField(`Gneres`, Anime.genres.join(", "))
-         .setThumbnail(Anime.picture)
-         .setFooter(`Score - ${Anime.score}`)
-         .setTimestamp();
- 
-     } catch (error) {
-       return message.channel.send(`No Anime Found!`);
-     };
- 
-     return message.channel.send(Embed);
-     //End
+    //Start
+
+    let Text = args[0];
+
+    if (!Text) return message.channel.send(`Please Give Something!`);
+
+    if (Text.length > 200) return message.channel.send(`Text Limit - 200`);
+
+    let Msg = await message.channel.send(`**Searching It For You 🔮**`);
+
+    let Replaced = Text.replace(/ +/g, " ");
+
+    await Msg.delete();
+
+    let Anime;
+
+    let Embed;
+
+    try {
+
+      Anime = await Scraper.getInfoFromName(Replaced);
+
+      if (!Anime.genres[0] || Anime.genres[0] === null) Anime.genres[0] = "None";
+
+      Embed = new Discord.MessageEmbed()
+        .setColor('#2f3136')
+        .setURL(Anime.url)
+        .setTitle(Anime.title)
+        .setDescription(Anime.synopsis)
+        .addField(`Type`, Anime.type, true)
+        .addField(`Status`, Anime.status, true)
+        .addField(`Premiered`, Anime.premiered, true)
+        .addField(`Episodes`, Anime.episodes, true)
+        .addField(`Duration`, Anime.duration, true)
+        .addField(`Popularity`, Anime.popularity, true)
+        .addField(`Gneres`, Anime.genres.join(", "))
+        .setThumbnail(Anime.picture)
+        .setFooter(`Score - ${Anime.score}`)
+        .setTimestamp();
+
+    } catch (error) {
+      return message.channel.send(`No Anime Found!`);
+    };
+
+    return message.channel.send(Embed);
+    //End
   }
 }
---
+++ src/commands/utilities/UserinfoCommand.js
@@ -0,0 +1,83 @@
+const BaseCommand = require('../../utils/structures/BaseCommand');
+const {
+  MessageEmbed
+} = require("discord.js")
+const moment = require("moment")
+
+module.exports = class UserinfoCommand extends BaseCommand {
+  constructor() {
+    super('userinfo', 'utilities', ['whois', 'ui']);
+  }
+
+  async run(client, message, args) {
+    let member;
+    if (!args.length) {
+      // Display info about the calling user
+      member = message.guild.member(message.author);
+    } else {
+      // Display info about the user specified by the first argument
+      member = message.guild.member(message.mentions.users.first());
+
+      // Check we were able to retrieve the member (member is undefined)
+      if (!member) {
+        return message.reply(
+          `I couldn't find a user with the ID \`${args[0]}\``
+        );
+      }
+    }
+    // Format Permissions
+    const permissions = member.permissions.toArray().map(perm => {
+      return perm
+        .toLowerCase()
+        .replace(/_/g, " ") // Replace all underscores with spaces
+        .replace(/\w\S*/g, txt => {
+          // Capitalize the first letter of each word
+          return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+        });
+    });
+
+    // Calculate Join Position
+    let joinPosition;
+    const members = message.guild.members.cache.array();
+    members.sort((a, b) => a.joinedAt - b.joinedAt);
+    for (let i = 0; i < members.length; i++) {
+      if (members[i].id == message.guild.member(message.author).id)
+        joinPosition = i;
+    }
+
+    // Construct Reply
+    const embed = {
+      embed: {
+        color: 3447003,
+        title: `${member.user.tag}`,
+        thumbnail: {
+          url: member.user.avatarURL()
+        },
+        description: `${member.displayName}`,
+        fields: [{
+            name: "Created",
+            value: `${member.user.createdAt.toDateString()} at ${member.user.createdAt.toTimeString()}`
+          },
+          {
+            name: "Joined",
+            value: `${member.joinedAt.toDateString()} at ${member.joinedAt.toTimeString()}`
+          },
+          {
+            name: "Join Position",
+            value: joinPosition
+          },
+          {
+            name: "Permissions",
+            value: permissions.join(", ")
+          }
+        ],
+        timestamp: new Date(),
+        footer: {
+          text: `ID: ${member.id}`
+        }
+      }
+    };
+
+    return message.channel.send(embed);
+  }
+}
