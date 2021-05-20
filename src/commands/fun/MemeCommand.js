ab4ab9d6b2607cfd2fcfaead378e45e235165fd6
Meema <78662983+japandotorg@users.noreply.github.com>
2021-05-20 15:41:40 +0530

    added a meme command 😹

--- src/commands/fun/MemeCommand.js
+++ src/commands/fun/MemeCommand.js
@@ -1,11 +1,26 @@
 const BaseCommand = require('../../utils/structures/BaseCommand');
+const fetch = require('node-fetch');
 
 module.exports = class MemeCommand extends BaseCommand {
   constructor() {
     super('meme', 'fun', []);
   }
 
-  run(client, message, args) {
-    message.channel.send('meme command works');
+  async run(client, message, args) {
+    const data = await fetch(`https://meme-api.herokuapp.com/gimme`)
+      .then(res => res.json())
+      .catch(() => null);
+
+    if (!data) {
+      return message.channel.send(`Server Error 5xx: Meme API is currently down!`);
+    }
+
+    return message.channel.send(
+      new MessageEmbed()
+      .setColor('GREY')
+      .setImage(data.url)
+      .setAuthor(data.title, null, data.postLink)
+      .setFooter(`${data.subreddit}:Meme | Made with ❤ by Lemon`)
+    );
   }
-}
+};
