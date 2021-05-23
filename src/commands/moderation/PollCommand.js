fc8b690396946d4cb2ccb1994e839f3a8e3cea01
Meema <78662983+japandotorg@users.noreply.github.com>
2021-05-23 22:34:47 +0530

    Fixed poll command 💀

--- src/commands/moderation/PollCommand.js
+++ src/commands/moderation/PollCommand.js
@@ -1,5 +1,6 @@
 const BaseCommand = require('../../utils/structures/BaseCommand');
-const Discord = require('discord.js')
+const Discord = require('discord.js');
+
 
 module.exports = class PollCommand extends BaseCommand {
   constructor() {
@@ -7,31 +8,44 @@ module.exports = class PollCommand extends BaseCommand {
   }
 
   async run(client, message, args) {
-    if (!message.member.hasPermission("MANAGE_SERVER")) return message.channel.send("**||Beya dekhats|| cannot use this command**");
-    if (!args.join(" ")) return message.reply("Please enter a question for the poll!")
-
-    message.delete();
-
-    let firstPollEmbed = new Discord.MessageEmbed()
-      .setAuthor(message.author.username, message.author.avatarURL({ dynamic: true }))
-      .setColor('#f4c2c2')
-      .setDescription("⌛ Preparing a poll...")
-    let secondPollEmbed = new Discord.MessageEmbed()
-      .setAuthor(`${message.author.tag}`, message.author.avatarURL({ dynamic: true }))
-      .setColor('#f4c2c2')
-      .setTitle("```\n📥 POLL:\n```")
-      .setDescription(`**${args.join(" ")}**`)
-    message.channel.send(firstPollEmbed).then((message) => {
-      setTimeout(function () {
-        message.edit(secondPollEmbed).then(sentMessage => {
-         sentMessage.react('✅')
-         sentMessage.react('❎')
-        }).catch(error => {
-          message.channel.send(`:x: **| There was an error executing your command:**\n\`${error}\``)
+    if (!message.member.hasPermission("ADMINISTRATOR")) return message.reply("You're not allowed to use this command!") //remove this line of code to allow everyone to use
+    const pollChannel = message.channel
+    const pollTitle = message.content.split(' ').splice(1).join(' ');
+    if (!pollTitle) return message.reply("Specify a Title!");
+    pollTitle.replace(" ", " ");
+    const pollEmbed1 = new Discord.MessageEmbed()
+        .setTitle("Creating Poll...")
+        .setColor("#ff8c00")
+    const pollEmbed2 = new Discord.MessageEmbed()
+        .setTitle("Sending Poll...")
+        .setColor("#ff8c00")
+    const pollEmbed3 = new Discord.MessageEmbed()
+        .setTitle("Poll Send...")
+        .setDescription("**Adding Reactions**")
+        .setColor("#ff8c00")
+    const pollEmbed4 = new Discord.MessageEmbed()
+        .setTitle("Reactions Added")
+        .setColor("#ff8c00")
+    message.channel.send(pollEmbed1).then(poll => {
+        const pollStatus = poll
+        const pollEmbed = new Discord.MessageEmbed()
+            .setTitle("Poll")
+            .setDescription(`${pollTitle}`)
+            .setTimestamp()
+            .setColor("BLUE")
+        pollStatus.edit(pollEmbed2)
+        pollChannel.send(pollEmbed).then(embed => {
+            pollStatus.edit(pollEmbed3)
+            embed.react("👍")
+            setTimeout(() => {
+                embed.react("👎")
+                pollStatus.edit(pollEmbed4)
+                setTimeout(() => {
+                    pollStatus.delete()
+                    message.delete()
+                }, 6000);
+            }, 2300)
         })
-      }, 2000)
-    }).catch(error => {
-      message.channel.send(`:x: **| There was an error executing your command:**\n\`${error}\``)
     })
-  }
+}
 }
