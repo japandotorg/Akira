a55ac0c6ac8dbfb7139bded68e7c968302b10e74
Meema <78662983+japandotorg@users.noreply.github.com>
2021-05-24 00:35:39 +0530

    added warnings command

---
+++ src/commands/moderation/WarningsCommand.js
@@ -0,0 +1,18 @@
+const BaseCommand = require('../../utils/structures/BaseCommand');
+const db = require("quick.db");
+
+module.exports = class WarningsCommand extends BaseCommand {
+  constructor() {
+    super('warnings', 'moderation', []);
+  }
+
+  run(client, message, args) {
+    const user = message.mentions.members.first() || message.author;
+
+    let warnings = db.get(`warnings_${message.guild.id}_${user.id}`);
+
+    if (warnings === null) warnings = 0;
+
+    message.channel.send(`${user} have **${warnings}** warning(s)`);
+  }
+}
