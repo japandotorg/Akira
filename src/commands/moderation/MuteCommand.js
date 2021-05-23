4ed87ea8ff46039bc28f1416008ba52be130436a
Meema <78662983+japandotorg@users.noreply.github.com>
2021-05-23 19:02:39 +0530

    added set mod log channel command 🐈

--- src/commands/moderation/MuteCommand.js
+++ src/commands/moderation/MuteCommand.js
@@ -1,5 +1,7 @@
 const BaseCommand = require('../../utils/structures/BaseCommand');
-const { MessageEmbed } = require("discord.js");
+const {
+  MessageEmbed
+} = require("discord.js");
 const db = require('quick.db');
 
 
@@ -24,85 +26,86 @@ module.exports = class MuteCommand extends BaseCommand {
       let reason = args.slice(1).join(" ");
       if (mutee.user.bot) return message.channel.send("**Cannot Mute Bots!**");
       const userRoles = mutee.roles.cache
-          .filter(r => r.id !== message.guild.id)
-          .map(r => r.id)
+        .filter(r => r.id !== message.guild.id)
+        .map(r => r.id)
 
       let muterole;
       let dbmute = await db.fetch(`muterole_${message.guild.id}`);
       let muteerole = message.guild.roles.cache.find(r => r.name === "Muted")
 
       if (!message.guild.roles.cache.has(dbmute)) {
-          muterole = muteerole
+        muterole = muteerole
       } else {
-          muterole = message.guild.roles.cache.get(dbmute)
+        muterole = message.guild.roles.cache.get(dbmute)
       }
 
       if (!muterole) {
-          try {
-              muterole = await message.guild.roles.create({
-                  data: {
-                      name: "Muted",
-                      color: "#514f48",
-                      permissions: []
-                  }
-              })
-              message.guild.channels.cache.forEach(async (channel) => {
-                  await channel.createOverwrite(muterole, {
-                      SEND_MESSAGES: false,
-                      ADD_REACTIONS: false,
-                      SPEAK: false,
-                      CONNECT: false,
-                  })
-              })
-          } catch (e) {
-              console.log(e);
-          }
+        try {
+          muterole = await message.guild.roles.create({
+            data: {
+              name: "Muted",
+              color: "#514f48",
+              permissions: []
+            }
+          })
+          message.guild.channels.cache.forEach(async (channel) => {
+            await channel.createOverwrite(muterole, {
+              SEND_MESSAGES: false,
+              ADD_REACTIONS: false,
+              SPEAK: false,
+              CONNECT: false,
+            })
+          })
+        } catch (e) {
+          console.log(e);
+        }
       };
 
       if (mutee.roles.cache.has(muterole.id)) return message.channel.send("**User Is Already Muted!**")
 
       db.set(`muteeid_${message.guild.id}_${mutee.id}`, userRoles)
-    try {
-      mutee.roles.set([muterole.id]).then(() => {
+      try {
+        mutee.roles.set([muterole.id]).then(() => {
           mutee.send(`**Hello, You Have Been Muted In ${message.guild.name} for - ${reason || "No Reason"}`).catch(() => null)
-      })
+        })
       } catch {
-           mutee.roles.set([muterole.id])
+        mutee.roles.set([muterole.id])
       }
-          if (reason) {
-          const sembed = new MessageEmbed()
-              .setColor("GREEN")
-              .setAuthor(message.guild.name, message.guild.iconURL())
-              .setDescription(`${mutee.user.username} was successfully jailed for ${reason}`)
-          message.channel.send(sembed);
-          } else {
-              const sembed2 = new MessageEmbed()
-              .setColor("GREEN")
-              .setDescription(`${mutee.user.username} was successfully jailed`)
-          message.channel.send(sembed2);
-          }
+      if (reason) {
+        const sembed = new MessageEmbed()
+          .setColor("GREEN")
+          .setAuthor(message.guild.name, message.guild.iconURL())
+          .setDescription(`${mutee.user.username} was successfully jailed for ${reason}`)
+        message.channel.send(sembed);
+      } else {
+        const sembed2 = new MessageEmbed()
+          .setColor("GREEN")
+          .setDescription(`${mutee.user.username} was successfully jailed`)
+        message.channel.send(sembed2);
+      }
 
       let channel = db.fetch(`modlog_${message.guild.id}`)
       if (!channel) return;
 
       let embed = new MessageEmbed()
-          .setColor('RED')
-          .setThumbnail(mutee.user.displayAvatarURL({ dynamic: true }))
-          .setAuthor(`${message.guild.name} Modlogs`, message.guild.iconURL())
-          .addField("**Moderation**", "mute")
-          .addField("**Mutee**", mutee.user.username)
-          .addField("**Moderator**", message.author.username)
-          .addField("**Reason**", `${reason || "**No Reason**"}`)
-          .addField("**Date**", message.createdAt.toLocaleString())
-          .setFooter(message.member.displayName, message.author.displayAvatarURL())
-          .setTimestamp()
+        .setColor('RED')
+        .setThumbnail(mutee.user.displayAvatarURL({
+          dynamic: true
+        }))
+        .setAuthor(`${message.guild.name} Modlogs`, message.guild.iconURL())
+        .addField("**Moderation**", "mute")
+        .addField("**Mutee**", mutee.user.username)
+        .addField("**Moderator**", message.author.username)
+        .addField("**Reason**", `${reason || "**No Reason**"}`)
+        .addField("**Date**", message.createdAt.toLocaleString())
+        .setFooter(message.member.displayName, message.author.displayAvatarURL())
+        .setTimestamp()
 
       var sChannel = message.guild.channels.cache.get(channel)
       if (!sChannel) return;
       sChannel.send(embed)
-  } catch {
+    } catch {
       return;
+    }
   }
 }
-}
-
