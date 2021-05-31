3b195b1dc302aafd2a1f396fbfd7f6b495877723
Meema <78662983+japandotorg@users.noreply.github.com>
2021-06-01 02:12:38 +0530

    added eval 🦆

--- src/commands/owner/EvalCommand.js
+++ src/commands/owner/EvalCommand.js
@@ -1,11 +1,51 @@
 const BaseCommand = require('../../utils/structures/BaseCommand');
+const Discord = require('discord.js');
+const beautify = require('beautify');
 
 module.exports = class EvalCommand extends BaseCommand {
   constructor() {
     super('eval', 'owner', []);
   }
 
-  run(client, message, args) {
-    message.channel.send('eval command works');
+  async run(client, message, args) {
+    
+    if (message.author.id !== 759180080328081450) {
+      return;
   }
+  
+  if (!args[0]) {
+      return;
+  }
+
+  try{
+      if (args.join(" ").toLowerCase().includes("token")) {
+          return;
+      }
+  
+      const toEval = args.join(" ");
+      const evaluated = eval(toEval); 
+
+      let embed = new Discord.MessageEmbed()
+      .setColor("#00FF00")
+      .setTimestamp()
+      .setFooter(client.user.username)
+      .setTitle("Eval")
+      .addField("To Evaluate", `\`\`\`js\n${beautify(args.join(" "), { format: "js"})}\n\`\`\``)
+      .addField("Evaluated:", `\`\`\`${evaluated}\`\`\``)
+      .addField("Type of:", `\`\`\`${typeof(evaluated)}\`\`\``);
+
+      message.channel.send(embed);
+
+  } catch (e) {
+      let embed = new Discord.MessageEmbed()
+      .setColor("#FF0000")
+      .setTitle(`🔴 Error!`)
+      .setDescription(e)
+
+      message.channel.send(embed);
+
+  }
+
+}
+
 }
